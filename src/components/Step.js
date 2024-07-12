28e1273cc039e72cf1be3fd68a234f21cef1bba3
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-12 16:03:05 +0530

    third commit

--- src/components/Step.js
+++ src/components/Step.js
@@ -3,7 +3,7 @@ import React from "react";
 const Step = ({ step, handleChange, formData }) => {
   if (step === 1) {
     return (
-      <div id={`step${step}`} className="customer container">
+      <div id='step1' className="customer container">
         <h1>Customer Details</h1>
         <label htmlFor="first_name">First Name:</label>
         <input onChange={handleChange} id="first_name" name="first_name" value={formData.first_name} />
@@ -13,7 +13,7 @@ const Step = ({ step, handleChange, formData }) => {
     );
   } else if (step === 2) {
     return (
-      <div id={`step${step}`} className="car container">
+      <div id='step2' className="car container">
         <h1>Car Details</h1>
         <label htmlFor="model">Model:</label>
         <input onChange={handleChange} id="model" name="model" value={formData.model} />
@@ -23,7 +23,7 @@ const Step = ({ step, handleChange, formData }) => {
     );
   } else if (step === 3) {
     return (
-      <div id={`step${step}`} className="payment container">
+      <div id='step3' className="payment container">
         <h1>Payment Details</h1>
         <label htmlFor="card_info">Credit Card Number:</label>
         <input type="number" onChange={handleChange} id="card_info" name="card_info" value={formData.card_info} />
