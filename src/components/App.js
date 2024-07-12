c43a28b9c533091087c343e5cc7b40c0ad58903a
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-12 15:50:50 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,60 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Step from "./Step";
 
 const App = () => {
+  const [step, setStep] = useState(1); // Starts at step 1
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    model: "",
+    car_price: "",
+    card_info: "",
+    expiry_date: "",
+  }); // Initializes form data
+
+  function nextStep() {
+    setStep(step + 1);
+  }
+
+  function prevStep() {
+    setStep(step - 1);
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const newData = { ...formData };
+    newData[name] = value;
+    setFormData(newData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formData);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={step === 3 ? handleSubmit : (e) => e.preventDefault()}>
+        <Step step={step} handleChange={handleChange} formData={formData} />
+        {step !== 1 && (
+          <button onClick={prevStep} type="button">
+            Back
+          </button>
+        )}
+        {step !== 3 && (
+          <button onClick={nextStep} type="button">
+            Next
+          </button>
+        )}
+        {step === 3 && (
+          <button type="submit">
+            Submit
+          </button>
+        )}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Step.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Step = ({ step, handleChange, formData }) => {
+  if (step === 1) {
+    return (
+      <div id={step} className="customer container">
+        <h1>Customer Details</h1>
+        <label htmlFor="first_name">First Name:</label>
+        <input onChange={handleChange} id="first_name" name="first_name" value={formData.first_name} />
+        <label htmlFor="last_name">Last Name:</label>
+        <input onChange={handleChange} id="last_name" name="last_name" value={formData.last_name} />
+      </div>
+    );
+  } else if (step === 2) {
+    return (
+      <div id={step} className="car container">
+        <h1>Car Details</h1>
+        <label htmlFor="model">Model:</label>
+        <input onChange={handleChange} id="model" name="model" value={formData.model} />
+        <label htmlFor="car_price">Price:</label>
+        <input onChange={handleChange} type="number" id="car_price" name="car_price" value={formData.car_price} />
+      </div>
+    );
+  } else if (step === 3) {
+    return (
+      <div id={step} className="payment container">
+        <h1>Payment Details</h1>
+        <label htmlFor="card_info">Credit Card Number:</label>
+        <input type="number" onChange={handleChange} id="card_info" name="card_info" value={formData.card_info} />
+        <label htmlFor="expiry_date">Expiration Date:</label>
+        <input onChange={handleChange} type="date" id="expiry_date" name="expiry_date" value={formData.expiry_date} />
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default Step;
