1c87671d5be39c74420e1316b4a6a8b0869025b5
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-15 11:59:21 +0530

    sixth commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,13 @@
-import React, { useState, useEffect } from "react";
-import "./../styles/App.css";
+import React, { useState } from "react";
+import './../styles/App.css';
 import Step from "./Step";
-
 const App = () => {
-  const [step, setStep] = useState(1); // Starts at step 1
-  const [formData, setFormData] = useState({
-    first_name: "",
-    last_name: "",
-    model: "",
-    car_price: "",
-    card_info: "",
-    expiry_date: "",
-  }); // Initializes form data
-
-  function nextStep() {
-    setStep(step + 1);
-  }
-
-  function prevStep() {
-    setStep(step - 1);
-  }
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    const newData = { ...formData };
-    newData[name] = value;
-    setFormData(newData);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form submitted:', formData);
-  };
-
-  useEffect(() => {
-    console.log('Current step:', step);
-  }, [step]);
-
+  const [currentstep,setcurrentstep]=useState(['','hide','hide']);
   return (
     <div>
-      <form onSubmit={step === 3 ? handleSubmit : (e) => e.preventDefault()}>
-        <Step step={step} handleChange={handleChange} formData={formData} nextStep={nextStep} prevStep={prevStep} />
-      </form>
+      <Step currentstep={currentstep} setcurrentstep={setcurrentstep} />
+        {/* Do not remove the main div */}
     </div>
-  );
-};
-
-export default App;
+  )
+}
+export default App
